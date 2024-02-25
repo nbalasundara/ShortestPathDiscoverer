2d7174736c4114805eb815b694f8ac6dce3a5ace
De Bits <nbalasundara@gmail.com>
2024-02-25 13:34:31 -0500

    Update MatrixTests.swift
    Use space instead of ',' for separator

--- ShortestPathDiscoverer/ShortestPathDiscovererTests/MatrixTests.swift
+++ ShortestPathDiscoverer/ShortestPathDiscovererTests/MatrixTests.swift
@@ -34,7 +34,7 @@ final class ShortestPathDiscovererTests: XCTestCase {
     }
 
     func testFirstExampleCase() throws {
-        let testCase1 = "3,4,1,2,8,6\n6,1,8,2,7,4\n5,9,3,9,9,5\n8,4,1,3,2,6\n3,7,2,8,6,4"
+        let testCase1 = "3 4 1 2 8 6\n6 1 8 2 7 4\n5 9 3 9 9 5\n8 4 1 3 2 6\n3 7 2 8 6 4
         Task {
             let validPaths : Set = [[0,1,2,3,3,4], [1,2,3,4,4,5]]
             let m = try Loader.load(testCase1)
@@ -49,7 +49,7 @@ final class ShortestPathDiscovererTests: XCTestCase {
     }
 
     func testSecondExampleCase() throws {
-        let testCase1 = "3,4,1,2,8,6\n6,1,8,2,7,4\n5,9,3,9,9,5\n8,4,1,3,2,6\n3,7,2,1,2,3"
+        let testCase1 = "3 4 1 2 8 6\n6 1 8 2 7 4\n5 9 3 9 9 5\n8 4 1 3 2 6\n3 7 2 1 2 3"
         Task {
             let validPaths : Set = [[1,2,1,5,4,5], [0,1,0,4,3,4]]
             let m = try Loader.load(testCase1)
@@ -64,7 +64,7 @@ final class ShortestPathDiscovererTests: XCTestCase {
     }
     
     func testThirdExampleCase() throws {
-        let testCase1 = "19,10,19,10,19\n21,23,20,19,12\n20,12,20,11,10"
+        let testCase1 = "19 10 19 10 19\n21 23 20 19 12\n20 12 20 11 10"
         Task {
             let m = try Loader.load(testCase1)
             var isFound : Bool;
